eeeee56ca47b5efb74c59be3a92863bbf0e18cce
Valerio Cofano <cofano@medacta.ch>
2024-03-14 15:42:32 +0100

    feat: init client

---
+++ client/postcss.config.js
@@ -0,0 +1,10 @@
+// Modifica da CommonJS a ES Module
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
